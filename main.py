127260398e45cc8d420e26e02a47986675c3a83f
Matthew Latham <50916823+latham7@users.noreply.github.com>
2022-10-10 21:24:14 -0400

    refined main.py - adding central sum system
    currently not working. Just commiting so I can switch to my MacBook

--- main.py
+++ main.py
@@ -4,40 +4,54 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
+version = 1.1
+
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    return render_template("index.html",)
+    return render_template("index.html", version=version)
+
+@app.route('/calculate')
+def calculate():
+    goldValue = int(request.args.get('gold-amount'))
+    ironValue = int(request.args.get('iron-amount'))
+    coalValue = int(request.args.get('coal-amount'))
+    copperValue = int(request.args.get('copper-amount'))
+
+    goldOutput = goldValue*2275
+    ironOutput = ironValue*2275
+    coalOutput = coalValue*2275
+    copperOutput = copperValue*2275
+
+    total=goldOutput+ironOutput+coalOutput+copperOutput
+
+
+
+    return render_template("index.html", version=version, total=total)
 
 @app.route('/goldCalculate')
 def goldCalculate():
-    goldValue = request.args.get('gold-amount')
-    goldValueInt = int(goldValue)
-    goldValueOutput = goldValueInt*2275
+    goldValue = int(request.args.get('gold-amount'))
+    goldOutput = goldValue*2275
+    return render_template("index.html", version=version, goldValueOutput=goldOutput)
 
-
-
-    return render_template("index.html", goldValueOutput=goldValueOutput)
 
 @app.route('/ironCalculate')
 def ironCalculate():
-    ironValue = request.args.get('iron-amount')
-    ironValueInt = int(ironValue)
-    ironValueOutput = ironValueInt*2275
-    return render_template('index.html', ironValueOutput=ironValueOutput)
+    ironValue = int(request.args.get('iron-amount'))
+    ironOutput = ironValue*2275
+    return render_template("index.html", version=version, ironValueOutput=ironOutput)
 
 @app.route('/coalCalculate')
 def coalCalculate():
-    coalValue = request.args.get('coal-amount')
-    coalValueInt = int(coalValue)
-    coalValueOutput = coalValueInt*2275
-    return render_template('index.html', coalValueOutput=coalValueOutput)
+    coalValue = int(request.args.get('coal-amount'))
+    coalOutput = coalValue*2275
+    return render_template("index.html", version=version, coalValueOutput=coalOutput)
 
 @app.route('/copperCalculate')
 def copperCalculate():
-    copperValue = request.args.get('copper-amount')
-    copperValueInt = int(copperValue)
-    copperValueOutput = copperValueInt*2275
-    return render_template('index.html', copperValueOutput=copperValueOutput)
+    copperValue = int(request.args.get('copper-amount'))
+    copperOutput = copperValue*2275
+    return render_template("index.html", version=version, copperValueOutput=copperOutput)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
