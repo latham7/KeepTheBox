ed07d2b8c42da32817b82c52a1ed526ae4c323af
Vegetable <50916823+vegetable8@users.noreply.github.com>
2022-10-12 20:47:03 -0400

    moving functions out of main.py

--- main.py
+++ main.py
@@ -1,57 +1,25 @@
 from flask import Flask, render_template, request, redirect
-
-
+from func.calculate import calculate
 
 app = Flask(__name__)
 
-version = 1.1
+version = 1.2
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
     return render_template("index.html", version=version)
 
 @app.route('/calculate')
-def calculate():
+def calculateRoute():
     goldValue = int(request.args.get('gold-amount'))
     ironValue = int(request.args.get('iron-amount'))
     coalValue = int(request.args.get('coal-amount'))
     copperValue = int(request.args.get('copper-amount'))
 
-    goldOutput = goldValue*2275
-    ironOutput = ironValue*2275
-    coalOutput = coalValue*2275
-    copperOutput = copperValue*2275
-
-    total=goldOutput+ironOutput+coalOutput+copperOutput
-
-
-
+    total = calculate(goldValue, ironValue, coalValue, copperValue)
+    print(type(total))
     return render_template("index.html", version=version, total=total)
 
-@app.route('/goldCalculate')
-def goldCalculate():
-    goldValue = int(request.args.get('gold-amount'))
-    goldOutput = goldValue*2275
-    return render_template("index.html", version=version, goldValueOutput=goldOutput)
-
-
-@app.route('/ironCalculate')
-def ironCalculate():
-    ironValue = int(request.args.get('iron-amount'))
-    ironOutput = ironValue*2275
-    return render_template("index.html", version=version, ironValueOutput=ironOutput)
-
-@app.route('/coalCalculate')
-def coalCalculate():
-    coalValue = int(request.args.get('coal-amount'))
-    coalOutput = coalValue*2275
-    return render_template("index.html", version=version, coalValueOutput=coalOutput)
-
-@app.route('/copperCalculate')
-def copperCalculate():
-    copperValue = int(request.args.get('copper-amount'))
-    copperOutput = copperValue*2275
-    return render_template("index.html", version=version, copperValueOutput=copperOutput)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
