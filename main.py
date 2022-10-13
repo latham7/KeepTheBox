631f371c3885c922d2d0c150ccd9b3864d828fe5
Vegetable <50916823+vegetable8@users.noreply.github.com>
2022-10-12 21:02:39 -0400

    added protection against text in number field.

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+from ast import While
 from flask import Flask, render_template, request, redirect
 from func.calculate import calculate
 
 app = Flask(__name__)
 
-version = 1.2
+version = "1.1.1"
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
@@ -11,14 +12,16 @@ def index():
 
 @app.route('/calculate')
 def calculateRoute():
-    goldValue = int(request.args.get('gold-amount'))
-    ironValue = int(request.args.get('iron-amount'))
-    coalValue = int(request.args.get('coal-amount'))
-    copperValue = int(request.args.get('copper-amount'))
-
-    total = calculate(goldValue, ironValue, coalValue, copperValue)
-    print(type(total))
-    return render_template("index.html", version=version, total=total)
+    try:
+        goldValue = int(request.args.get('gold-amount'))
+        ironValue = int(request.args.get('iron-amount'))
+        coalValue = int(request.args.get('coal-amount'))
+        copperValue = int(request.args.get('copper-amount'))
+        total = calculate(goldValue, ironValue, coalValue, copperValue)
+        return render_template("index.html", version=version, total=total)
+    except ValueError:
+        print("0001 - Could not convert string to int")
+        return render_template("index.html", version=version, total="ERROR: Please enter a number.")
 
 
 if __name__ == '__main__':
