f01741d99a93d29750cc5e71dfd1ff9e7f7aebfc
Vegetable <50916823+vegetable8@users.noreply.github.com>
2022-10-12 21:19:43 -0400

    using post method instead of form args.

--- main.py
+++ main.py
@@ -6,20 +6,22 @@ app = Flask(__name__)
 
 version = "1.1.1"
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/", methods=['GET', 'POST']) ## This index is weird. It will be a login page at some point
 def index():
     return render_template("index.html", version=version)
 
-@app.route('/calculate')
+@app.route('/calculate', methods=['GET', 'POST'])
 def calculateRoute():
+    goldValueDebug = int(request.form.get('gold-amount'))
+    print(goldValueDebug)
     try:
-        goldValue = int(request.args.get('gold-amount'))
-        ironValue = int(request.args.get('iron-amount'))
-        coalValue = int(request.args.get('coal-amount'))
-        copperValue = int(request.args.get('copper-amount'))
+        goldValue = int(request.form.get('gold-amount'))
+        ironValue = int(request.form.get('iron-amount'))
+        coalValue = int(request.form.get('coal-amount'))
+        copperValue = int(request.form.get('copper-amount'))
         total = calculate(goldValue, ironValue, coalValue, copperValue)
         return render_template("index.html", version=version, total=total)
-    except ValueError:
+    except ValueError: #TypeError:
         print("0001 - Could not convert string to int")
         return render_template("index.html", version=version, total="ERROR: Please enter a number.")
 
