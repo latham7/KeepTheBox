32c1464ce8e60bf09356ff608484b379454d699b
Matthew Latham <50916823+latham7@users.noreply.github.com>
2022-10-22 17:33:00 -0400

    AUTHENTICATION IS WORKING- no cookies doe

--- auth.py
+++ auth.py
@@ -1,16 +1,27 @@
 import sqlite3
-from flask import request
+from flask import render_template, request
 
 con = sqlite3.connect('database.db', check_same_thread=False)
+cur = con.cursor()
 
 def checkAuth(username, password):
-    test = con.execute(f"SELECT * FROM Users WHERE username='{username}' AND password='{password}'")
-    print(test)
-    try:
-        con.execute(f"SELECT * FROM Users WHERE username='{ username }' AND password='{password}'")
-        return True
+    cur.execute(f"SELECT * FROM Users WHERE username='{username}' AND password='{password}'")
+    stored = cur.fetchall()
+    print(stored)
+    print(f"[('{ username }', '{ password }')]")
+    try :
+        if stored[0][0] == username:
+            if stored[0][1] == password:
+                return True
+        else:
+            return False
     except:
-        return False
+        print("Error - auth.py - CheckAuth function")
+#    try:
+#        con.execute(f"SELECT * FROM Users WHERE username='{ username }' AND password='{password}'")
+#        return True
+#    except:
+#        return False
 
 
 def checkCookie(cookie):
