2768c61e288066f0e85beda9cacba4d252c0bc29
Matthew Latham <50916823+latham7@users.noreply.github.com>
2022-10-22 18:08:30 -0400

    added setup script

---
+++ setup.py
@@ -0,0 +1,24 @@
+# 
+#  __     __              _        _     _      
+#  \ \   / /__  __ _  ___| |_ __ _| |__ | | ___ 
+#   \ \ / / _ \/ _` |/ _ \ __/ _` | '_ \| |/ _ \
+#    \ V /  __/ (_| |  __/ || (_| | |_) | |  __/
+#     \_/ \___|\__, |\___|\__\__,_|_.__/|_|\___|
+#              |___/                            
+#
+# Vegetable Development - https://github.com/Vegetable8
+# This is to setup parts of KeepTheBox Server. 
+
+# Imports
+import sqlite3
+
+## Vars
+con = sqlite3.connect('database.db', check_same_thread=False)
+
+# DB Init
+try:
+    con.execute("create table Users(username varchar(64),password varchar(64));")
+except:
+    print("Error - setup.py - Database is already initialized.")
+
+
